96e9cfe94f5d0325fcabf228bfcf648d3dd3b550
YongKunWang <wang_yongkun@yeah.net>
2020-07-29 11:52:10 +0800

    finish user Interface

--- src/router.js
+++ src/router.js
@@ -2,13 +2,28 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/Login.vue'
 import Home from './components/Home.vue'
+import Welcome from './components/Welcome.vue'
+import Users from './components/users/Users'
 Vue.use(Router)
+// 解决：Error: Avoided redundant navigation to current location
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 const router = new Router({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users }
+      ]
+    }
   ]
 })
 
