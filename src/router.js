9e64838c842bdcc6aaa7ab4e025596dd95e473ff
YongKunWang <wangyk1026@yeah.net>
2020-10-07 13:27:14 +0800

    完成订单功能

--- src/router.js
+++ src/router.js
@@ -9,7 +9,8 @@ import Roles from './components/power/roles.vue'
 import Category from './components/goods/Category.vue'
 import Params from './components/goods/Params.vue'
 import Goods from './components/goods/Goods.vue'
-import Add from './components/goods/Add'
+import Add from './components/goods/Add.vue'
+import Order from './components/order/Order.vue'
 Vue.use(Router)
 // 解决：Error: Avoided redundant navigation to current location
 const originalPush = Router.prototype.push
@@ -33,7 +34,8 @@ const router = new Router({
         { path: '/categories', component: Category },
         { path: '/params', component: Params },
         { path: '/goods', component: Goods },
-        { path: '/goods/add', component: Add }
+        { path: '/goods/add', component: Add },
+        { path: '/orders', component: Order }
       ]
     }
   ]
