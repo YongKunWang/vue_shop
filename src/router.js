7361bcbacd225f2b4615019d4b2b3b4b414d8b68
YongKunWang <wangyk1026@yeah.net>
2020-10-07 14:15:13 +0800

    完成数据报表

--- src/router.js
+++ src/router.js
@@ -11,6 +11,7 @@ import Params from './components/goods/Params.vue'
 import Goods from './components/goods/Goods.vue'
 import Add from './components/goods/Add.vue'
 import Order from './components/order/Order.vue'
+import Report from './components/report/Report'
 Vue.use(Router)
 // 解决：Error: Avoided redundant navigation to current location
 const originalPush = Router.prototype.push
@@ -35,7 +36,8 @@ const router = new Router({
         { path: '/params', component: Params },
         { path: '/goods', component: Goods },
         { path: '/goods/add', component: Add },
-        { path: '/orders', component: Order }
+        { path: '/orders', component: Order },
+        { path: '/reports', component: Report }
       ]
     }
   ]
